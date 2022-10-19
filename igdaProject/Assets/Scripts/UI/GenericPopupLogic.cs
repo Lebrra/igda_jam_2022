d7cc6f5ac8b55ec0d6dc7c3c3bb3ae8e7f0d98c7
Leah Blasczyk <scout1665@gmail.com>
2022-10-18 19:00:06 -0500

    Finished grayboxed generic popup and title screen

--- igdaProject/Assets/Scripts/UI/GenericPopupLogic.cs
+++ igdaProject/Assets/Scripts/UI/GenericPopupLogic.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using BeauRoutine;
 
 public class GenericPopupLogic : MonoBehaviour
 {
@@ -32,5 +33,73 @@ public class GenericPopupLogic : MonoBehaviour
     public void Close()
     {
         popupAnim.SetBool("Enabled", false);
+        Routine.Start(DelayClose());
+    }
+
+    IEnumerator DelayClose()
+    {
+        yield return new WaitForSeconds(0.5F);
+        popupPanel.SetActive(false);
+    }
+
+    public void FillContent(string prompt, Action yesAction, Action noAction, string yesPrompt = "Yes", string noPrompt = "No")
+    {
+        promptText.text = prompt;
+
+        yesButton.gameObject.SetActive(true);
+        yesButton.onClick.RemoveAllListeners();
+        if (yesAction != null) yesButton.onClick.AddListener(() => yesAction());
+        else yesButton.onClick.AddListener(Close);
+        yesText.text = yesPrompt;
+
+        noButton.gameObject.SetActive(true);
+        noButton.onClick.RemoveAllListeners();
+        if (noAction != null) noButton.onClick.AddListener(() => noAction());
+        else noButton.onClick.AddListener(Close);
+        noText.text = noPrompt;
+    }
+
+    public void FillContent(string prompt, Action endAction, string closePrompt = "Okay") 
+    {
+        if (closePrompt == null)
+        {
+            FillContent(prompt, endAction, () => noButton.onClick.AddListener(Close));
+            return;
+        }
+
+        promptText.text = prompt;
+
+        yesButton.gameObject.SetActive(true);
+        noButton.gameObject.SetActive(false);
+        yesButton.onClick.RemoveAllListeners();
+        if (endAction != null) yesButton.onClick.AddListener(() => endAction());
+        else yesButton.onClick.AddListener(Close);
+        yesText.text = closePrompt;
     }
 }
+
+/*
+ * Use example:
+ * 
+public class PopupExample : MonoBehaviour
+{
+    public GenericPopupLogic popup;
+
+    void UseSimplePopup()
+    {
+        popup.FillContent("this is a popup", null);
+        popup.Open();
+    }
+
+    void UseQuestionPopup()
+    {
+        popup.FillContent("this popup is a question", null, null);
+        popup.Open();
+    }
+}
+
+ * 
+ * 
+ * 
+ * 
+ */
