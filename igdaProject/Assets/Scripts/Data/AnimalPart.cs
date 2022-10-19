45085c4adb1f749c789bfc29e2f38b0c3744773e
Leah Blasczyk <scout1665@gmail.com>
2022-10-18 22:03:58 -0500

    Generating animal part objects

---
+++ igdaProject/Assets/Scripts/Data/AnimalPart.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public class AnimalPart : ScriptableObject
+{
+    #region Related Types
+    public enum BodyPart
+    {
+        Head,
+        Body,
+        Legs,
+        Tail,
+        None
+    }
+
+    [Serializable]
+    public struct AnimalPartData
+    {
+        public string id;
+        public string animal;
+        public string namePart;
+        public BodyPart bodyPart;
+        public string abilityName;
+        public int abilityCost;
+        public int health;
+    }
+    #endregion
+
+    public AnimalPartData partData;
+
+    #region Asset Creation/Parsing
+    void LoadData(AnimalPartData newPartData)
+    {
+        partData.id = newPartData.id;
+        partData.animal = newPartData.animal;
+        partData.namePart = newPartData.namePart;
+        partData.bodyPart = newPartData.bodyPart;
+        partData.abilityName = newPartData.abilityName;
+        partData.abilityCost = newPartData.abilityCost;
+        partData.health = newPartData.health;
+    }
+
+    public static void CreateAsset(AnimalPartData newPartData)
+    {
+        AnimalPart asset = CreateInstance<AnimalPart>();
+        asset.LoadData(newPartData);
+        AssetDatabase.CreateAsset(asset, "Assets/Data/AnimalParts/" + asset.partData.id + ".asset");
+        Debug.Log("Asset created: " + asset.partData.id);
+    }
+
+    public static BodyPart StringToPart(string partString)
+    {
+        switch (partString.ToLower().Trim(' '))
+        {
+            case "head": return BodyPart.Head;
+            case "body": return BodyPart.Body;
+            case "legs": return BodyPart.Legs;
+            case "tail": return BodyPart.Tail;
+        }
+        return BodyPart.None;
+    }
+    #endregion
+}
---
+++ igdaProject/Assets/Scripts/Data/DataImporter.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+#endif
+
+#if UNITY_EDITOR
+public class DataImporter : Editor
+{
+    [MenuItem("Custom Editor/Reload Animal Parts")]
+    public static void ReloadParts()
+    {
+        Debug.Log("Importing Data...");
+
+        StreamReader sr = new StreamReader(Application.dataPath + "/Data/partData.csv");
+        sr.ReadLine();  // remove headers
+
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine().Split(',');
+            for (int i = 0; i < line.Length; i++) line[i] = line[i].Trim(' ');
+            AnimalPart.CreateAsset(ArrayToAnimalPartData(line));
+        }
+
+        Debug.Log("Completed import.");
+    }
+
+    //string[] headerNames = new string[8] { "id", "Animal", "Part", "AnimalNamePart", "Biome", "AbilityName", "AbilityCost", "Health" };
+    static AnimalPart.AnimalPartData ArrayToAnimalPartData(string[] lineList)
+    {
+        AnimalPart.AnimalPartData newPartData;
+
+        newPartData.id = lineList[0];
+        newPartData.animal = lineList[1];
+        newPartData.namePart = lineList[3];
+        newPartData.bodyPart = AnimalPart.StringToPart(lineList[2]);
+        newPartData.abilityName = lineList[5];
+        int.TryParse(lineList[6], out newPartData.abilityCost);
+        int.TryParse(lineList[7], out newPartData.health);
+
+        return newPartData;
+    }
+}
+#else
+public class DataImporter { }
+#endif
