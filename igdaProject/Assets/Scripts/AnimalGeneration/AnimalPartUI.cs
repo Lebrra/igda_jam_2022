58d2d99870a2024b026a6907a6b689877e99c144
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2022-10-20 20:23:47 -0500

    Adding UI to parts and updating the UI object in the Resources folder.

--- igdaProject/Assets/Scripts/AnimalGeneration/AnimalPartUI.cs
+++ igdaProject/Assets/Scripts/AnimalGeneration/AnimalPartUI.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AnimalPartUI : MonoBehaviour
 {
     public string id;
     public AnimalPart myPart;
-    public Sprite animalIcon;
+    public Image animalIcon;
     public void FindMyPart(string myID)
     {
         id = myID;
