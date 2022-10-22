5faa4571e495f494a72666cf69566ac63200fd40
Leah Blasczyk <scout1665@gmail.com>
2022-10-22 14:31:42 -0500

    nonsense I need to stop messing with

--- igdaProject/Assets/Scripts/UI/SettingsManager.cs
+++ igdaProject/Assets/Scripts/UI/SettingsManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Toggle sfxButton;
+    [SerializeField]
+    Toggle musicButton;
+    [SerializeField]
+    Button titleButton;
+
+    private void Start()
     {
-        
+        sfxButton.onValueChanged.AddListener(SetSFX);
+        sfxButton.isOn = GameManager.instance.playerdata.sfxEnabled;
+
+        musicButton.onValueChanged.AddListener(SetMusic);
+        musicButton.isOn = GameManager.instance.playerdata.musicEnabled;
+
+        titleButton.onClick.AddListener(() =>
+        {
+            GameManager.ToTitle();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open()
     {
-        
+
+    }
+
+    public void Close()
+    {
+
+    }
+
+    void SetSFX(bool enabled)
+    {
+        GameManager.instance.playerdata.sfxEnabled = enabled;
+    }
+
+    void SetMusic(bool enabled)
+    {
+        GameManager.instance.playerdata.musicEnabled = enabled;
     }
 }
